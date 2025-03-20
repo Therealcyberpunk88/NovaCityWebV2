1d94a4379784fef5f89a6c318fa6924599ecf8cf
Therealcyberpunk88 <118804039+Therealcyberpunk88@users.noreply.github.com>
2025-03-20 12:34:16 +0100

    first commit

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2096055.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 200770911, 200770919, 200770921, 200770935, 200770938, 200770945, 200770947, 200770948 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/200770908/1/basis.wasm.js', 'wasmUrl' : 'files/assets/200770909/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/200770907/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/200770904/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/200770905/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/200770903/1/ammo.js', 'preload' : true},
+];
